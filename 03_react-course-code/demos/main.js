83d1890ee84777a42a13494870ab04203a4bc664
Nithishkumar K R <nithishkumarkr@gmail.com>
2025-01-28 12:53:10 +0530

    Day1 and Day2 commits

---
+++ 03_react-course-code/demos/main.js
@@ -0,0 +1,17 @@
+const rootElement = document.getElementById("root");
+
+const element = (
+  <ul>
+    <li>
+      <a href="">a</a>
+    </li>
+    <li>
+      <a href="">b</a>
+    </li>
+    <li>
+      <a href="">c</a>
+    </li>
+  </ul>
+);
+
+ReactDOM.createRoot(rootElement).render(element);
---
+++ 04_projectpilot/src/App.tsx
@@ -0,0 +1,11 @@
+import ProjectsPage from "./projects/ProjectPage";
+
+function App() {
+  return (
+    <div className="container">
+      <ProjectsPage />
+    </div>
+  );
+}
+
+export default App;
