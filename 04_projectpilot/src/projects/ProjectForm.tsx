a79d2eef0605e1c4827e6911b2222d25ced9fa77
Nithishkumar K R <nithishkumarkr@gmail.com>
2025-01-30 10:25:47 +0530

    day4: onsave integration

--- 04_projectpilot/src/projects/ProjectForm.tsx
+++ 04_projectpilot/src/projects/ProjectForm.tsx
@@ -1,10 +1,19 @@
+import { SyntheticEvent } from "react";
+import { Project } from "./Project";
+
 interface ProjectFormProps {
   onCanel: () => void;
+  onSave: (project: Project) => void;
 }
 
-function ProjectForm({ onCanel }: ProjectFormProps) {
+function ProjectForm({ onCanel, onSave }: ProjectFormProps) {
+  const handleSubmit = (event: SyntheticEvent) => {
+    event.preventDefault();
+    onSave(new Project({ name: "Updated Project" }));
+  };
+
   return (
-    <form className="input-group vertical">
+    <form className="input-group vertical" onSubmit={handleSubmit}>
       <label htmlFor="name">Project Name</label>
       <input type="text" name="name" placeholder="enter name" />
       <label htmlFor="description">Project Description</label>
--- 04_projectpilot/src/projects/ProjectPage.tsx
+++ 04_projectpilot/src/projects/ProjectPage.tsx
@@ -1,11 +1,15 @@
-import { MOCK_PROJECTS } from "./MockProject";
+import { MOCK_PROJECTS } from "./mockproject";
+import { Project } from "./Project";
 import ProjectList from "./ProjectList";
 
 function ProjectsPage() {
+  const saveProject = (project: Project) => {
+    console.log("Saving Project", project);
+  };
   return (
     <>
       <h1>Projects</h1>
-      <ProjectList projects={MOCK_PROJECTS} />
+      <ProjectList projects={MOCK_PROJECTS} onSave={saveProject} />
     </>
   );
 }
